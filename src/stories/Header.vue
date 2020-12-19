06e7b545dfad4d2cd44a345da88fbd0229dfa201
arunstop <arunstop@gmail.com>
2020-12-19 16:58:41 +0700

    search button minimized

--- src/stories/Header.vue
+++ src/stories/Header.vue
@@ -1,44 +1,45 @@
 <template>
-    <div class="c-navbar">
-      <div class="c-wrapper">
-        <img
-          class="logo"
-          src="https://assets.website-files.com/5fafb178b3236f251c2ec605/5fafe519c3163b6fd26dd449_logo-color.svg"
-        />
-        <input
-          type="text"
-          class="btn-search"
-          maxlength="256"
-          name="Search"
-          data-name="Search"
-          placeholder="Search products"
-          id="Search"
-        />
-        <div class="g-right row">
-          <!-- <my-button
+  <div class="c-navbar">
+    <div class="c-wrapper">
+      <img
+        class="logo"
+        src="https://assets.website-files.com/5fafb178b3236f251c2ec605/5fafe519c3163b6fd26dd449_logo-color.svg"
+      />
+      <input
+        type="text"
+        class="btn-search"
+        maxlength="256"
+        name="Search"
+        data-name="Search"
+        placeholder="Search products"
+        id="Search"
+      />
+      <div class="g-right row">
+        <!-- <my-button
             size="small"
             @onClick="onLogout"
             label="Log out"
             v-if="user"
           /> -->
-          <my-button
-            size="small"
-            @onClick="onLogin"
-            label="Log in"
-            v-if="!user"
-          />
-          <my-button
-            primary
-            size="small"
-            @onClick="onCreateAccount"
-            label="Sign up"
-            v-if="!user"
-          />
-          <my-sidebar/>
-          <c-icon-button icon="person" icon-hovered="person-fill"/>
-        </div>
+        <my-button
+          size="small"
+          @onClick="onLogin"
+          label="Log in"
+          v-if="!user"
+        />
+        <my-button
+          primary
+          size="small"
+          @onClick="onCreateAccount"
+          label="Sign up"
+          v-if="!user"
+        />
+        <c-icon-button icon="search" icon-hovered="search" />
+        <my-sidebar />
+        <c-icon-button icon="person" icon-hovered="person-fill" />
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -51,12 +52,12 @@ import CIconButton from "./IconButton.vue";
 export default {
   name: "my-header",
 
-  components: { MyButton,MySidebar,CIconButton },
+  components: { MyButton, MySidebar, CIconButton },
 
   props: {
     user: {
-      type: String
-    }
+      type: String,
+    },
   },
 
   methods: {
@@ -69,6 +70,6 @@ export default {
     onCreateAccount() {
       this.$emit("onCreateAccount");
     },
-  }
+  },
 };
 </script>
