daef7aab1fa8b0d163ea9d0dfc3938686a191e92
arunstop <arunstop@gmail.com>
2021-01-01 04:00:42 +0700

    moving globals to plugins

--- src/main.js
+++ src/main.js
@@ -5,45 +5,27 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'animate.css'
 
-import Vuex from 'vuex';
+// import Vuex from 'vuex';
 import store from './store'
-import globals from './stories/globals';
+import globals from './plugins/globals';
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import { longClickDirective } from 'vue-long-click'
-import Vue2Storage from 'vue2-storage'
+// import Vue2Storage from 'vue2-storage'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-Vue.use(Vuex)
-Vue.use(Vue2Storage, {
-  prefix: 'app_',
-  driver: 'local',
-  ttl: 60 * 60 * 24 * 1000 // 24 hours
-})
+// Vue.use(Vuex)
+// Vue.use(Vue2Storage, {
+//   prefix: 'app_',
+//   driver: 'local',
+//   ttl: 60 * 60 * 24 * 1000 // 24 hours
+// })
 
 
 Vue.directive('long-click', longClickDirective({ delay: 600, interval: 120 }))
 
-// reauthenticate user if local user data exists
-var localUserData = Vue.$storage.get('userData', null)
-if (localUserData != null) {
-  store.dispatch("userSignIn", localUserData);
-}
-
-// add cart data if it exists in local storage
-var localCartData = Vue.$storage.has('cartData')
-
-if (localCartData) {
-  // Adding cart data from local storage
-  // console.log(Vue.$storage.get('cartData').data)
-  let cartData = Vue.$storage.get('cartData').data
-  cartData.map(
-    item => store.dispatch('addToCart', item)
-  )
-  // console.log(store.getters.getCartData)
-}
 
 const vm = new Vue({
   render: h => h(App),
@@ -52,7 +34,29 @@ const vm = new Vue({
   store,
 })
 
+
+// re-authenticate user if local user data exists
+var localUserData = vm.$storage.get('userData', null)
+if (localUserData != null) {
+  store.dispatch("userSignIn", localUserData);
+}
+
+// add cart data if it exists in local storage
+var localCartData = vm.$storage.has('cartData')
+
+if (localCartData) {
+  // Adding cart data from local storage
+  // console.log(vm.$storage.get('cartData').data)
+  let cartData = vm.$storage.get('cartData').data
+  cartData.map(
+    item => store.dispatch('addToCart', item)
+  )
+  // console.log(store.getters.getCartData)
+}
+
 //global is for statics variables
 Vue.prototype.$globals = new globals(vm)
+// console.log(vm.$storage.prefix)
 
 vm.$mount('#app')
+
